423062f5df4ba4d13421b243d733ee0e210ed4d0
u172798 <72573703+u172798@users.noreply.github.com>
2022-05-24 13:29:06 +0200

    edit lab

---
+++ Build-a-rail/Assets/scripts/build-a-rail/StoneController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider targetObj)
+    {
+        if (targetObj.gameObject.tag == "Player1" && GameController.pickaxe_p1_status)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (targetObj.gameObject.tag == "Player2" && GameController.pickaxe_p2_status)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
